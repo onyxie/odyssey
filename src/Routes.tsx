ef164108a6a727cfdea56a5a9332d60fb13b94d9
onyxie <mand_ogoshi@hotmail.com>
2023-05-26 00:32:53 -0300

    adjust

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes as Routing } from "react-router-dom"
 import Home from "./pages/Home"
+import NavBar from "./components/navbar"
 
 const Routes = () => {
     return (
+        <>
+        <NavBar></NavBar>
         <BrowserRouter>
             <Routing>
                 <Route path='/' element={<Home />}></Route>
             </Routing>
         </BrowserRouter>
+        </>
     )
 }
 
